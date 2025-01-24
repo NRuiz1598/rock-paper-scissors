e7f029585096b74b925dc2f8a797da491b742dfb
NRuiz1598 <nruiz1512@gmail.com>
2025-01-24 12:04:56 -0500

    Add Value to Each Button.

--- script.js
+++ script.js
@@ -3,16 +3,19 @@ const body  = document.querySelector('body');
 const rock = document.createElement('button');
 rock.textContent = "Rock";
 rock.classList.add("option");
+rock.value = "Rock"
 body.appendChild(rock);
 
 const paper = document.createElement('button');
 paper.textContent = "Paper";
 paper.classList.add("option")
+paper.value = "Paper"
 body.appendChild(paper);
 
 const scissors = document.createElement('button');
 scissors.textContent = "Scissors";
-scissors.classList.add("option")
+scissors.classList.add("option");
+scissors.value = "Scissors";
 body.appendChild(scissors);
 
 function getComputerChoice() {
